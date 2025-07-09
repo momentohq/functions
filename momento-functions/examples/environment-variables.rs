6709ae049f146cdfb20fd621e53bc6bba73c0b78
Dylan Abraham <dylan@momentohq.com>
2025-07-09 11:27:12 -0700

    chore: clippy

--- momento-functions/examples/environment-variables.rs
+++ momento-functions/examples/environment-variables.rs
@@ -4,7 +4,6 @@ use momento_functions::WebResponse;
 momento_functions::post!(env);
 fn env(_payload: Vec<u8>) -> WebResponse {
     std::env::vars()
-        .into_iter()
         .map(|(k, v)| format!("{k}={v}"))
         .join("\n")
         .into()

146e2749b5df5d5b788d91eba8fcd69b3785396a
Tyler Burdsall <10414594+tylerburdsall@users.noreply.github.com>
2025-09-19 10:33:36 -0700

    feat: update WIT with new guest function web helper. add example function. (#19)

--- momento-functions-host/src/web_extensions.rs
+++ momento-functions-host/src/web_extensions.rs
@@ -5,9 +5,18 @@
 use momento_functions_wit::function_web::momento::functions::web_function_support;
 
 /// Returns the headers for the web function, if any are present.
+/// This consumes the headers and takes ownership of the value; multiple calls after will always
+/// yield `None`.
 pub fn headers() -> Vec<(String, String)> {
     web_function_support::headers()
         .into_iter()
         .map(|web_function_support::Header { name, value }| (name, value))
         .collect()
+}
+
+/// Returns the metadata within the caller's token, if present.
+/// This consumes the metadata and takes ownership of the value; multiple calls after will always
+/// yield `None`.
+pub fn token_metadata() -> Option<String> {
+    web_function_support::token_metadata()
 }

77b3e5346a0159d1aac2052cf62d613b12d6378e
Tyler Burdsall <10414594+tylerburdsall@users.noreply.github.com>
2025-12-11 12:28:54 -0800

    feat: add query_parameters() call for web extensions (#29)

--- momento-functions-host/src/web_extensions.rs
+++ momento-functions-host/src/web_extensions.rs
@@ -6,11 +6,21 @@ use momento_functions_wit::function_web::momento::functions::web_function_suppor
 
 /// Returns the headers for the web function, if any are present.
 /// This consumes the headers and takes ownership of the value; multiple calls after will always
-/// yield `None`.
+/// yield an empty list.
 pub fn headers() -> Vec<(String, String)> {
     web_function_support::headers()
         .into_iter()
         .map(|web_function_support::Header { name, value }| (name, value))
+        .collect()
+}
+
+/// Returns the query parameters for the web function, if any are present.
+/// This consumes the parameters and takes ownership of the value; multiple calls after will always
+/// yield an empty list.
+pub fn query_parameters() -> Vec<(String, String)> {
+    web_function_support::query_parameters()
+        .into_iter()
+        .map(|web_function_support::QueryParameter { name, value }| (name, value))
         .collect()
 }
 
